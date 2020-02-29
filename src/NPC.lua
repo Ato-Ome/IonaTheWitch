9e56d98ee591d21b9f20bc63c1a967b9128baa89
Феникс <ato_Ome@mail.ru>
2020-02-29 06:01:19 +0600

    NPC Create Function
    NPC Create Function created

--- src/NPC.lua
+++ src/NPC.lua
@@ -1,39 +1,70 @@
 NPC = {
-    state = {
-        pathfind = false,
-        walk = false,
-        busy = false,
-        idle = false,
-        attack = false,
-        sleep = false,
-        dead = false
+    State = {
+        Pathfind = false,
+        Walk = false,
+        Busy = false,
+        Idle = false,
+        Attack = false,
+        Sleep = false,
+        Dead = false
     },
-    fraction = {
-        aggressive = false,
-        negative = false,
-        neutral = false,
-        positive = false,
-        friendly = false
+    Fraction = {
+        Aggressive = false,
+        Negative = false,
+        Neutral = false,
+        Positive = false,
+        Friendly = false
     },
-    character = {
-        sex = {
-            male = false,
-        },
-        age = {
-            child = false,
-            adult = false,
-            old = false
-        },
-        class = {
-            assassin = false,
-            bard = false,
-            barmen = false,
-            blacksmith = false,
-            cook = false,
-            captain = false,
-
+    Character = {
+        Sex = {
+            Male = false,
+            Female = true
+        }
+        Age = {},
+        Class = {
+            Assassin = {
+                Id = FourCC(''),
+                Boolean = true,
+            }false,
+            Bandit = false,
+            Bard = false,
+            Barmen = false,
+            Blacksmith = false,
+            Butcher = false,
+            Cook = false,
+            Captain = false,
+            Courtesan = false,
+            Doctor = false,
+            Farmer = false,
+            Fisherman = false,
+            Gravedigger = false,
+            Governor = false,
+            Innkeeper = false,
+            Jailer = false,
+            Knight = false,
+            Logger = false,
+            Mercenary = false,
+            Merchant = false,
+            Monk = false,
+            Rifleman = false,
+            Traveller = false,
+            Villager = false
         }
     }
 }
 
-BlzGetTriggerFrameText(ORIGIN_FRAME_TOOLTIP)
+---@param x real
+---@param y real
+---@param fraction integer
+---@param sex integer
+---@param age integer
+---@param class integer
+---
+function CreateNPC(x, y, fraction, sex, age, class)
+    local unit = CreateUnit(GetPlayerId(),FourCC(''),x,y,0)
+    local data = NPC[UnitId(unit)]
+    data.Character.Sex.sex = true
+    data.Character.Age.age
+end
+
+CreateNPC(0, 0, 1, 1,1, 1)
